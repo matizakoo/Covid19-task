902e88cc2e26d59fbb155041412f3cc03c6d4e23
Mateusz Żak <matzakoo@gmail.com>
2022-09-21 22:36:01 +0200

    Initial commit

---
+++ src/main/java/com/company/Covid19task/Covid19TaskApplication.java
@@ -0,0 +1,13 @@
+package com.company.Covid19task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19TaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Covid19task/Covid19TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Covid19task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
